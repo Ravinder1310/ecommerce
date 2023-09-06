05d2e0801e64e66fbff5d0099e840982f1177993
Ravinder1310 <105969256+Ravinder1310@users.noreply.github.com>
2023-09-06 11:15:50 +0530

    header and footer layout ready

---
+++ ecommerce_app/src/pages/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/layouts/Layout'
+
+const Landing = () => {
+  return (
+    <div>
+        <Layout>
+        <h1>Homepage</h1>
+        </Layout>
+    </div>
+  )
+}
+
+export default Landing
